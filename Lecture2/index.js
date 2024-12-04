f25a0c7d80a5a9d86492c507764db7a2bd1b20c7
earnny34 <s6606021611202@kmutnb.ac.th>
2024-12-04 14:21:04 +0700

    lecture2

---
+++ Lecture2/index.js
@@ -0,0 +1,10 @@
+const message = "Hello";
+const place  = "World";
+//git config 
+// logging out a string
+console.log('Hello,Word!') 
+// using substitutions
+console.log('hello, %s!',place);
+
+//using a string literal
+console.log('${message},${place}!');
